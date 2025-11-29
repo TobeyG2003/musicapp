bd2b034f184381825478c238ea5fc64de4c7f31a
Tobey <TobeyG2003@gmail.com>
2025-11-28 20:44:46 -0500

    adds firebase setup, pubspec packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+import 'dart:convert';
+import 'dart:typed_data';
+import 'package:image_picker/image_picker.dart';
+
 
 void main() {
   runApp(const MyApp());
